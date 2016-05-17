6b5d90cc2f166c6a8b7e1ad286d7c527c169bf6d
Antonio Álvarez <analca3@gmail.com>
2016-05-17 23:59:58 +0200

    Arreglado procedimiento de extracción de datos

--- Consultas/extractReplays.sql
+++ Consultas/extractReplays.sql
@@ -12,6 +12,9 @@ CREATE TABLE result (
         GroundUnitValue INT NOT NULL,
         BuildingValue INT NOT NULL,
         AirUnitValue INT NOT NULL,
+        AObservedEnemyGroundUnitValue INT NOT NULL,
+        AObservedEnemyBuildingValue INT NOT NULL,
+        AObservedEnemyAirUnitValue INT NOT NULL,
         EnemyMinerals INT NOT NULL,
         EnemyGas INT NOT NULL,
         EnemySupply INT NOT NULL,
@@ -21,11 +24,16 @@ CREATE TABLE result (
         EnemyGroundUnitValue INT NOT NULL,
         EnemyBuildingValue INT NOT NULL,
         EnemyAirUnitValue INT NOT NULL,
-        ResourceValue INT NOT NULL,
-        EnemyResourceValue INT NOT NULL,
-        Winner INT NOT NULL        
+        BObservedEnemyGroundUnitValue INT NOT NULL,
+        BObservedEnemyBuildingValue INT NOT NULL,
+        BObservedEnemyAirUnitValue INT NOT NULL,
+        AObservedResourceValue INT NOT NULL,
+        BObservedResourceValue INT NOT NULL,
+        Winner CHAR NOT NULL,
+
+        PRIMARY KEY (ReplayID, Frame)
     );
-    
+
 DROP PROCEDURE extractReplay;
 
 DELIMITER //
@@ -37,10 +45,10 @@ BEGIN
 	DECLARE PlayerB INT;
 	DECLARE repDuration INT;
 
-	SELECT A.PlayerReplayID
-		,B.PlayerReplayID
-	INTO PlayerA
-		,PlayerB
+	SELECT A.PlayerReplayID,
+		B.PlayerReplayID
+	INTO PlayerA,
+		PlayerB
 	FROM (
 		SELECT PlayerReplayID
 		FROM playerreplay pr
@@ -62,81 +70,93 @@ BEGIN
 
 	-- Extract one replay
 	INSERT INTO result (
-		ReplayID
-		,Duration
-		,Frame
-		,Minerals
-		,Gas
-		,Supply
-		,TotalMinerals
-		,TotalGas
-		,TotalSupply
-		,GroundUnitValue
-		,BuildingValue
-		,AirUnitValue
-		,EnemyMinerals
-		,EnemyGas
-		,EnemySupply
-		,EnemyTotalMinerals
-		,EnemyTotalGas
-		,EnemyTotalSupply
-		,EnemyGroundUnitValue
-		,EnemyBuildingValue
-		,EnemyAirUnitValue
-		,ResourceValue
-		,EnemyResourceValue
-		,Winner
+		ReplayID,
+		Duration,
+		Frame,
+		Minerals,
+		Gas,
+		Supply,
+		TotalMinerals,
+		TotalGas,
+		TotalSupply,
+		GroundUnitValue,
+		BuildingValue,
+		AirUnitValue,
+		AObservedEnemyGroundUnitValue,
+		AObservedEnemyBuildingValue,
+		AObservedEnemyAirUnitValue,
+		AObservedResourceValue,
+		EnemyMinerals,
+		EnemyGas,
+		EnemySupply,
+		EnemyTotalMinerals,
+		EnemyTotalGas,
+		EnemyTotalSupply,
+		EnemyGroundUnitValue,
+		EnemyBuildingValue,
+		EnemyAirUnitValue,
+		BObservedEnemyGroundUnitValue,
+		BObservedEnemyBuildingValue,
+		BObservedEnemyAirUnitValue,
+		BObservedResourceValue,
+		Winner
 		)
-	SELECT IDReplay
-		,repDuration
-		,AB.Frame
-		,@minerals := IFNULL(Minerals, @minerals) AS Minerals
-		,@gas := IFNULL(Gas, @gas) AS Gas
-		,@supply := IFNULL(Supply, @supply) AS Supply
-		,@totMinerals := IFNULL(TotalMinerals, @totMinerals) AS TotalMinerals
-		,@totGas := IFNULL(TotalGas, @totGas) AS TotalGas
-		,@totSupply := IFNULL(TotalSupply, @totSupply) AS TotalSupply
-		,@ground := IFNULL(GroundUnitValue, @ground) AS GroundUnitValue
-		,@building := IFNULL(BuildingValue, @building) AS BuildingValue
-		,@air := IFNULL(AirUnitValue, @air) AS AirUnitValue
-		,@enemyMinerals := IFNULL(EnemyMinerals, @enemyMinerals) AS EnemyMinerals
-		,@enemyGas := IFNULL(EnemyGas, @enemyGas) AS EnemyGas
-		,@enemySupply := IFNULL(EnemySupply, @enemySupply) AS EnemySupply
-		,@enemyTotMinerals := IFNULL(EnemyTotalMinerals, @enemyTotMinerals) AS EnemyTotalMinerals
-		,@enemyTotGas := IFNULL(EnemyTotalGas, @enemyTotGas) AS EnemyTotalGas
-		,@enemyTotSupply := IFNULL(EnemyTotalSupply, @enemyTotSupply) AS EnemyTotalSupply
-		,@enemyGround := IFNULL(EnemyGroundUnitValue, @enemyGround) AS EnemyGroundUnitValue
-		,@enemyBuilding := IFNULL(EnemyBuildingValue, @enemyBuilding) AS EnemyBuildingValue
-		,@enemyAir := IFNULL(EnemyAirUnitValue, @enemyAir) AS EnemyAirUnitValue
-		,@resourcesA := IFNULL(C.ResourceValue, @resourcesA) AS ResourceValue
-		,@resourcesB := IFNULL(D.ResourceValue, @resourcesB) AS EnemyResourceValue
-		,@winnerA := IFNULL(WinnerA, @winnerA) AS WinnerA
+	SELECT IDReplay,
+		repDuration,
+		AB.Frame,
+        @minerals := IFNULL(Minerals, @minerals) AS Minerals,
+		@gas := IFNULL(Gas, @gas) AS Gas,
+		@supply := IFNULL(Supply, @supply) AS Supply,
+		@totMinerals := IFNULL(TotalMinerals, @totMinerals) AS TotalMinerals,
+		@totGas := IFNULL(TotalGas, @totGas) AS TotalGas,
+		@totSupply := IFNULL(TotalSupply, @totSupply) AS TotalSupply,
+		@ground := IFNULL(GroundUnitValue, @ground) AS GroundUnitValue,
+		@building := IFNULL(BuildingValue, @building) AS BuildingValue,
+		@air := IFNULL(AirUnitValue, @air) AS AirUnitValue,
+		@aObservedEnemyGround := IFNULL(AObservedEnemyGroundUnitValue, @aObservedEnemyGround) AS AObservedEnemyGroundUnitValue,
+		@aObservedEnemyBuilding := IFNULL(AObservedEnemyBuildingValue, @aObservedEnemyBuilding) AS AObservedEnemyBuildingValue,
+		@aObservedEnemyAir := IFNULL(AObservedEnemyAirUnitValue, @aObservedEnemyAir) AS AObservedEnemyAirUnitValue,
+		@resourcesA := IFNULL(AObservedResourceValue, @resourcesA) AObservedResourceValue,
+		@enemyMinerals := IFNULL(EnemyMinerals, @enemyMinerals) AS EnemyMinerals,
+		@enemyGas := IFNULL(EnemyGas, @enemyGas) AS EnemyGas,
+		@enemySupply := IFNULL(EnemySupply, @enemySupply) AS EnemySupply,
+		@enemyTotMinerals := IFNULL(EnemyTotalMinerals, @enemyTotMinerals) AS EnemyTotalMinerals,
+		@enemyTotGas := IFNULL(EnemyTotalGas, @enemyTotGas) AS EnemyTotalGas,
+		@enemyTotSupply := IFNULL(EnemyTotalSupply, @enemyTotSupply) AS EnemyTotalSupply,
+		@enemyGround := IFNULL(GroundUnitValue, @enemyGround) AS GroundUnitValue,
+		@enemyBuilding := IFNULL(EnemyBuildingValue, @enemyBuilding) AS EnemyBuildingValue,
+		@enemyAir := IFNULL(EnemyAirUnitValue, @enemyAir) AS EnemyAirUnitValue,
+		@bObservedEnemyGround := IFNULL(AObservedEnemyGroundUnitValue, @bObservedEnemyGround) AS BObservedEnemyGroundUnitValue,
+		@bObservedEnemyBuilding := IFNULL(AObservedEnemyBuildingValue, @bObservedEnemyBuilding) AS BObservedEnemyBuildingValue,
+		@bObservedEnemyAir := IFNULL(AObservedEnemyAirUnitValue, @bObservedEnemyAir) AS BObservedEnemyAirUnitValue,
+		@resourcesB := IFNULL(BObservedResourceValue, @resourcesB) AS BObservedResourceValue,
+        @winnerA := IFNULL(WinnerA, @winnerA) AS WinnerA
 	FROM (
-		SELECT IFNULL(A.Frame, B.Frame) AS Frame
-			,A.Minerals
-			,A.Gas
-			,A.Supply
-			,A.TotalMinerals
-			,A.TotalGas
-			,A.TotalSupply
-			,A.Winner AS WinnerA
-			,B.Minerals AS EnemyMinerals
-			,B.Gas AS EnemyGas
-			,B.Supply AS EnemySupply
-			,B.TotalMinerals AS EnemyTotalMinerals
-			,B.TotalGas AS EnemyTotalGas
-			,B.TotalSupply AS EnemyTotalSupply
-			,B.Winner AS WinnerB
+		SELECT IFNULL(A.Frame, B.Frame) AS Frame,
+			A.Minerals,
+			A.Gas,
+			A.Supply,
+			A.TotalMinerals,
+			A.TotalGas,
+			A.TotalSupply,
+			CASE WHEN A.Winner=1 THEN 'A' ELSE 'B' END AS WinnerA,
+			B.Minerals AS EnemyMinerals,
+			B.Gas AS EnemyGas,
+			B.Supply AS EnemySupply,
+			B.TotalMinerals AS EnemyTotalMinerals,
+			B.TotalGas AS EnemyTotalGas,
+			B.TotalSupply AS EnemyTotalSupply,
+			B.Winner AS WinnerB
 		FROM (
-			SELECT rc.Frame
-				,pr.PlayerReplayID
-				,rc.Minerals
-				,rc.Gas
-				,rc.Supply
-				,rc.TotalMinerals
-				,rc.TotalGas
-				,rc.TotalSupply
-				,pr.Winner
+			SELECT rc.Frame,
+				pr.PlayerReplayID,
+				rc.Minerals,
+				rc.Gas,
+				rc.Supply,
+				rc.TotalMinerals,
+				rc.TotalGas,
+				rc.TotalSupply,
+				pr.Winner
 			FROM resourcechange rc
 			INNER JOIN playerreplay pr
 				ON pr.PlayerReplayID = rc.PlayerReplayID
@@ -148,15 +168,15 @@ BEGIN
 			ORDER BY rc.Frame ASC
 			) AS A
 		LEFT JOIN (
-			SELECT rc.Frame
-				,pr.PlayerReplayID
-				,rc.Minerals
-				,rc.Gas
-				,rc.Supply
-				,rc.TotalMinerals
-				,rc.TotalGas
-				,rc.TotalSupply
-				,pr.Winner
+			SELECT rc.Frame,
+				pr.PlayerReplayID,
+				rc.Minerals,
+				rc.Gas,
+				rc.Supply,
+				rc.TotalMinerals,
+				rc.TotalGas,
+				rc.TotalSupply,
+				pr.Winner
 			FROM resourcechange rc
 			INNER JOIN playerreplay pr
 				ON pr.PlayerReplayID = rc.PlayerReplayID
@@ -169,34 +189,34 @@ BEGIN
 			) AS B
 			ON A.Frame = B.Frame
 				AND A.PlayerReplayID != B.PlayerReplayID
-		
+
 		UNION ALL
-		
-		SELECT IFNULL(A.Frame, B.Frame) AS Frame
-			,A.Minerals
-			,A.Gas
-			,A.Supply
-			,A.TotalMinerals
-			,A.TotalGas
-			,A.TotalSupply
-			,A.Winner AS WinnerA
-			,B.Minerals
-			,B.Gas
-			,B.Supply
-			,B.TotalMinerals AS EnemyTotalMinerals
-			,B.TotalGas AS EnemyTotalGas
-			,B.TotalSupply AS EnemyTotalSupply
-			,B.Winner AS WinnerB
+
+		SELECT IFNULL(A.Frame, B.Frame) AS Frame,
+			A.Minerals,
+			A.Gas,
+			A.Supply,
+			A.TotalMinerals,
+			A.TotalGas,
+			A.TotalSupply,
+			A.Winner AS WinnerA,
+			B.Minerals,
+			B.Gas,
+			B.Supply,
+			B.TotalMinerals AS EnemyTotalMinerals,
+			B.TotalGas AS EnemyTotalGas,
+			B.TotalSupply AS EnemyTotalSupply,
+			B.Winner AS WinnerB
 		FROM (
-			SELECT rc.Frame
-				,pr.PlayerReplayID
-				,rc.Minerals
-				,rc.Gas
-				,rc.Supply
-				,rc.TotalMinerals
-				,rc.TotalGas
-				,rc.TotalSupply
-				,pr.Winner
+			SELECT rc.Frame,
+				pr.PlayerReplayID,
+				rc.Minerals,
+				rc.Gas,
+				rc.Supply,
+				rc.TotalMinerals,
+				rc.TotalGas,
+				rc.TotalSupply,
+				pr.Winner
 			FROM resourcechange rc
 			INNER JOIN playerreplay pr
 				ON pr.PlayerReplayID = rc.PlayerReplayID
@@ -208,15 +228,15 @@ BEGIN
 			ORDER BY rc.Frame ASC
 			) AS A
 		RIGHT JOIN (
-			SELECT rc.Frame
-				,pr.PlayerReplayID
-				,rc.Minerals
-				,rc.Gas
-				,rc.Supply
-				,rc.TotalMinerals
-				,rc.TotalGas
-				,rc.TotalSupply
-				,pr.Winner
+			SELECT rc.Frame,
+				pr.PlayerReplayID,
+				rc.Minerals,
+				rc.Gas,
+				rc.Supply,
+				rc.TotalMinerals,
+				rc.TotalGas,
+				rc.TotalSupply,
+				pr.Winner
 			FROM resourcechange rc
 			INNER JOIN playerreplay pr
 				ON pr.PlayerReplayID = rc.PlayerReplayID
@@ -232,14 +252,14 @@ BEGIN
 		WHERE A.Frame IS NULL
 		) AS AB
 	LEFT JOIN (
-		SELECT DISTINCT rc.Frame
-			,sum(GroundUnitValue) AS GroundUnitValue
-			,sum(BuildingValue) AS BuildingValue
-			,sum(AirUnitValue) AS AirUnitValue
-			,sum(EnemyGroundUnitValue) AS EnemyGroundUnitValue
-			,sum(EnemyBuildingValue) AS EnemyBuildingValue
-			,sum(EnemyAirUnitValue) AS EnemyAirUnitValue
-			,sum(ResourceValue) AS ResourceValue
+		SELECT DISTINCT rc.Frame,
+			sum(GroundUnitValue) AS GroundUnitValue,
+			sum(BuildingValue) AS BuildingValue,
+			sum(AirUnitValue) AS AirUnitValue,
+			sum(EnemyGroundUnitValue) AS AObservedEnemyGroundUnitValue,
+			sum(EnemyBuildingValue) AS AObservedEnemyBuildingValue,
+			sum(EnemyAirUnitValue) AS AObservedEnemyAirUnitValue,
+			sum(ResourceValue) AS AObservedResourceValue
 		FROM resourcechange rc
 		INNER JOIN regionvaluechange AS R1
 			ON rc.PlayerReplayID = R1.PlayerReplayID
@@ -252,14 +272,20 @@ BEGIN
 					AND Frame <= rc.Frame
 				GROUP BY RegionID
 				)
-		-- and rc.Frame > 0 
+		-- and rc.Frame > 0
 		GROUP BY rc.Frame
 		ORDER BY rc.Frame
 		) AS C
 		ON AB.Frame = C.Frame
 	LEFT JOIN (
-		SELECT DISTINCT rc.Frame
-			,sum(ResourceValue) AS ResourceValue
+		SELECT DISTINCT rc.Frame,
+			sum(GroundUnitValue) AS EnemyGroundUnitValue,
+			sum(BuildingValue) AS EnemyBuildingValue,
+			sum(AirUnitValue) AS EnemyAirUnitValue,
+			sum(EnemyGroundUnitValue) AS BObservedEnemyGroundUnitValue,
+			sum(EnemyBuildingValue) AS BObservedEnemyBuildingValue,
+			sum(EnemyAirUnitValue) AS BObservedEnemyAirUnitValue,
+			sum(ResourceValue) AS BObservedResourceValue
 		FROM resourcechange rc
 		INNER JOIN regionvaluechange AS R1
 			ON rc.PlayerReplayID = R1.PlayerReplayID
@@ -272,34 +298,40 @@ BEGIN
 					AND Frame <= rc.Frame
 				GROUP BY RegionID
 				)
-		-- and rc.Frame > 0 
+		-- and rc.Frame > 0
 		GROUP BY rc.Frame
 		ORDER BY rc.Frame
 		) AS D
 		ON AB.Frame = D.Frame
 	CROSS JOIN (
-		SELECT @minerals := 0
-			,@gas := 0
-			,@supply := 0
-			,@enemyMinerals := 0
-			,@enemyGas := 0
-			,@enemySupply := 0
-			,@totMinerals := 0
-			,@totGas := 0
-			,@totSupply := 0
-			,@winnerA := 0
-			,@enemyTotMinerals := 0
-			,@enemyTotGas := 0
-			,@enemyTotSupply := 0
-			,@winnerB := 0
-			,@ground := 0
-			,@building := 0
-			,@air := 0
-			,@enemyGround := 0
-			,@enemyBuilding := 0
-			,@enemyAir := 0
-			,@resourcesA := 0
-			,@resourcesB := 0
+		SELECT @minerals := 0,
+			@gas := 0,
+			@supply := 0,
+			@enemyMinerals := 0,
+			@enemyGas := 0,
+			@enemySupply := 0,
+			@totMinerals := 0,
+			@totGas := 0,
+			@totSupply := 0,
+			@winnerA := 0,
+			@enemyTotMinerals := 0,
+			@enemyTotGas := 0,
+			@enemyTotSupply := 0,
+			@winnerB := 0,
+			@ground := 0,
+			@building := 0,
+			@air := 0,
+			@aObservedEnemyGround := 0,
+			@aObservedEnemyBuilding := 0,
+			@aObservedEnemyAir := 0,
+			@enemyGround := 0,
+			@enemyBuilding := 0,
+			@enemyAir := 0,
+			@bObservedEnemyGround := 0,
+			@bObservedEnemyBuilding := 0,
+			@bObservedEnemyAir := 0,
+			@resourcesA := 0,
+			@resourcesB := 0
 		) AS var_init
 	ORDER BY AB.Frame;
 END;
@@ -318,18 +350,18 @@ BEGIN
 
 	DECLARE done INT DEFAULT FALSE;
     DECLARE C_Replay CURSOR FOR SELECT ReplayID FROM replay;
-                                
+
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-    
+
 	OPEN C_Replay;
-    
+
     replay_loop: LOOP
 		FETCH C_Replay INTO n_replay;
-        
+
         IF done THEN LEAVE replay_loop; END IF;
-        
+
         CALL `sc_pvp`.`extractReplay`(n_replay);
-    
+
     END LOOP replay_loop;
 END;
 //
